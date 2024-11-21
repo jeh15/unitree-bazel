e093bd9be5acd57306f84cb727c05634969cadd7
jeh15 <jeh15@fsu.edu>
2024-11-21 17:12:54 -0500

    TODO(jeh15): Create Python Bindings for wrapper class.

--- examples/custom/controller.cc
+++ examples/custom/controller.cc
@@ -21,6 +21,7 @@ using namespace unitree::robot;
 
 constexpr double PosStopF = (2.146E+9f);
 constexpr double VelStopF = (16000.0f);
+constexpr uint8_t num_motors = 12;
 
 std::map<std::string, uint8_t> MotorID = {
     {"Front_Right_Abduction", 0},
@@ -38,21 +39,23 @@ std::map<std::string, uint8_t> MotorID = {
 };
 
 struct MotorCommand {
-    float q_setpoint[12] = {
+    std::array<float, num_motors> q_setpoint = {
         0.0, 0.9, -1.8,
         0.0, 0.9, -1.8,
         0.0, 0.9, -1.8,
         0.0, 0.9, -1.8
     };
-    float qd_setpoint[12] = { 0 };
-    float torque_feedforward[12] = { 0 };
-    float kp[12] = { 0 };
-    float kd[12] = { 0 };
+    std::array<float, num_motors> qd_setpoint = { 0 };
+    std::array<float, num_motors> torque_feedforward = { 0 };
+    std::array<float, num_motors> stiffness = { 0 };
+    std::array<float, num_motors> damping = { 0 };
+    std::array<float, num_motors> kp = { 0 };
+    std::array<float, num_motors> kd = { 0 };
 };
 
 struct MotorState {
-    float q[12] = { 0 };
-    float qd[12] = { 0 };
+    std::array<float, num_motors> q = { 0 };
+    std::array<float, num_motors> qd = { 0 };
 };
 
 class MotorController {
@@ -61,6 +64,7 @@ public:
     ~MotorController() {}
     void init();
     void update_command(MotorCommand& motor_cmd);
+    void get_state(const std::string& motor_name);
 
 private:
     void init_cmd_msg();
@@ -71,44 +75,43 @@ private:
 
 private:
     const struct {
-        float q_lb[12] = {
+        std::array<float, num_motors> q_lb = {
             -1.0472, -1.5708, -2.7227,
             -1.0472, -1.5708, -2.7227,
             -1.0472, -0.5236, -2.7227,
             -1.0472, -0.5236, -2.7227
         };
-        float q_ub[12] = {
+        std::array<float, num_motors> q_ub = {
             1.0472, 3.4907, -0.83776,
             1.0472, 3.4907, -0.83776,
             1.0472, 4.5379, -0.83776,
             1.0472, 4.5379, -0.83776,
         };
-        float qd_lb[12] = {
+        std::array<float, num_motors> qd_lb = {
             -0.5, -0.5, -0.5,
             -0.5, -0.5, -0.5,
             -0.5, -0.5, -0.5,
             -0.5, -0.5, -0.5
         };
-        float qd_ub[12] = {
+        std::array<float, num_motors> qd_ub = {
             0.5, 0.5, 0.5,
             0.5, 0.5, 0.5,
             0.5, 0.5, 0.5,
             0.5, 0.5, 0.5
         };
-        float tau_lb[12] = {
+        std::array<float, num_motors> tau_lb = {
             -5.0, -5.0, -5.0,
             -5.0, -5.0, -5.0,
             -5.0, -5.0, -5.0,
             -5.0, -5.0, -5.0
         };
-        float tau_ub[12] = {
+        std::array<float, num_motors> tau_ub = {
             5.0, 5.0, 5.0,
             5.0, 5.0, 5.0,
             5.0, 5.0, 5.0,
             5.0, 5.0, 5.0
         };
     } motor_bounds;
-    std::string target = "Front_Right_Abduction";
 
     unitree_go::msg::dds_::LowCmd_ motor_cmd{};
     unitree_go::msg::dds_::LowState_ robot_state{};
@@ -195,17 +198,21 @@ void MotorController::robot_state_msg_handler(const void* message) {
 void MotorController::control_loop() {
     // Iterate over motors:
     for(const auto& [key, value] : MotorID) {
-        float torque_cmd = std::clamp(motor_commands.torque_feedforward[value], motor_bounds.tau_lb[value], motor_bounds.tau_ub[value]);
+        float q_error = motor_commands.q_setpoint[value] - robot_state.motor_state()[value].q();
+        float qd_error = motor_commands.qd_setpoint[value] - robot_state.motor_state()[value].dq();
+        float torque_feedforward = std::clamp(motor_commands.torque_feedforward[value], motor_bounds.tau_lb[value], motor_bounds.tau_ub[value]);
+        float torque_input = torque_feedforward + motor_commands.kp[value] * (q_error) + motor_commands.kd[value] * (qd_error);
+        float torque_cmd = std::clamp(torque_input,  motor_bounds.tau_lb[value], motor_bounds.tau_ub[value]);
         motor_cmd.motor_cmd()[value].q() = motor_commands.q_setpoint[value];
         motor_cmd.motor_cmd()[value].dq() = motor_commands.qd_setpoint[value];
-        motor_cmd.motor_cmd()[value].kp() = motor_commands.kp[value];
-        motor_cmd.motor_cmd()[value].kd() = motor_commands.kd[value];
+        motor_cmd.motor_cmd()[value].kp() = motor_commands.stiffness[value];
+        motor_cmd.motor_cmd()[value].kd() = motor_commands.damping[value];
         motor_cmd.motor_cmd()[value].tau() = torque_cmd;
     }
 
     // Checksum:
     motor_cmd.crc() = crc32_core((uint32_t *)&motor_cmd, (sizeof(unitree_go::msg::dds_::LowCmd_)>>2)-1);
-    
+
     // Publish Command:
     motor_cmd_publisher->Write(motor_cmd);
 }
@@ -216,14 +223,23 @@ void MotorController::update_command(MotorCommand& motor_cmd) {
         float q_setpoint = std::clamp(motor_cmd.q_setpoint[value], motor_bounds.q_lb[value], motor_bounds.q_ub[value]);
         float qd_setpoint = std::clamp(motor_cmd.qd_setpoint[value], motor_bounds.qd_lb[value], motor_bounds.qd_ub[value]);
         float torque_feedforward = std::clamp(motor_cmd.torque_feedforward[value], motor_bounds.tau_lb[value], motor_bounds.tau_ub[value]);
-        float kp = std::clamp(motor_cmd.kp[value], 0.0f, 100.0f);
-        float kd = std::clamp(motor_cmd.kd[value], 0.0f, 100.0f);
+        float stiffness = std::clamp(motor_cmd.stiffness[value], 0.0f, 20.0f);
+        float damping = std::clamp(motor_cmd.damping[value], 0.0f, 10.0f);
+        float kp = std::clamp(motor_cmd.kp[value], 0.0f, 10.0f);
+        float kd = std::clamp(motor_cmd.kd[value], 0.0f, 10.0f);
         motor_commands.q_setpoint[value] = q_setpoint;
         motor_commands.qd_setpoint[value] = qd_setpoint; 
         motor_commands.torque_feedforward[value] = torque_feedforward;
-        motor_commands.kp[value] = kp[value];
-        motor_commands.kd[value] = kd[value];
+        motor_commands.stiffness[value] = stiffness;
+        motor_commands.damping[value] = damping;
+        motor_commands.kp[value] = kp;
+        motor_commands.kd[value] = kd;
     }
+}
+
+void MotorController::get_state(const std::string& motor_name) {
+    auto motor_position = robot_state.motor_state()[MotorID[motor_name]].q();
+    std::cout << motor_name << " : " << motor_position << std::endl;
 }
 
 int main(int argc, const char** argv) {
@@ -243,18 +259,50 @@ int main(int argc, const char** argv) {
     const uint8_t dt = 20;
     MotorCommand motor_command;
 
+    // Set Motor Command:
+    motor_command.stiffness = {
+        60.0, 60.0, 60.0,
+        60.0, 60.0, 60.0,
+        60.0, 60.0, 60.0,
+        60.0, 60.0, 60.0
+    };
+    motor_command.damping = {
+        5.0, 5.0, 5.0,
+        5.0, 5.0, 5.0,
+        5.0, 5.0, 5.0,
+        5.0, 5.0, 5.0
+    };
+    motor_command.kp = {
+        10.0, 10.0, 10.0,
+        10.0, 10.0, 10.0,
+        10.0, 10.0, 10.0,
+        10.0, 10.0, 10.0
+    };
+    motor_command.kd = {
+        2.0, 2.0, 2.0,
+        2.0, 2.0, 2.0,
+        2.0, 2.0, 2.0,
+        2.0, 2.0, 2.0
+    };
+
+    // Update Controller Setpoints and Values:
+    controller.update_command(motor_command);
+
     while (true) {
         auto elapsed_time = std::chrono::high_resolution_clock::now() - start;
         // Send MotorController Command:
-        // controller.update_command(motor_command);
+
+        // Get state:
+        std::string key = "Front_Right_Abduction";
+        controller.get_state(key);
 
         // Sleep for dt:
         std::this_thread::sleep_for(std::chrono::milliseconds(dt));
 
         // Run for 10 Seconds:
-        if (std::chrono::duration_cast<std::chrono::duration<double>>(elapsed_time).count() > 10.0) {
+        if (std::chrono::duration_cast<std::chrono::duration<double>>(elapsed_time).count() > 60.0)
             break;
-        }
+        
     }
 
     return 0;
