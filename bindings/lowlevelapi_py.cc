5300bc862989d09a820ea0209ef464360fbdd525
jeh15 <jeh15@fsu.edu>
2025-01-10 13:20:25 -0500

    Unitree bindings work. TODO(jeh115): Run tests for parameter regression.

--- bindings/lowlevelapi_py.cc
+++ bindings/lowlevelapi_py.cc
@@ -28,7 +28,28 @@ PYBIND11_MODULE(unitree_api, m) {
         .def_readwrite("q", &lowleveltypes::MotorState::q)
         .def_readwrite("qd", &lowleveltypes::MotorState::qd)
         .def_readwrite("qdd", &lowleveltypes::MotorState::qdd)
-        .def_readwrite("torque_estimate", &lowleveltypes::MotorState::torque_estimate);
+        .def_readwrite("torque_estimate", &lowleveltypes::MotorState::torque_estimate)
+        .def(py::pickle(
+            [](const lowleveltypes::MotorState& obj) {
+                return py::make_tuple(obj.q, obj.qd, obj.qdd, obj.torque_estimate);
+            },
+            [](py::tuple t) {
+                if (t.size() != 4)
+                    throw std::runtime_error("Invalid state tuple");
+                lowleveltypes::MotorState obj;
+                obj.q = t[0].cast<std::array<float, lowleveltypes::num_motors>>();
+                obj.qd = t[1].cast<std::array<float, lowleveltypes::num_motors>>();
+                obj.qdd = t[2].cast<std::array<float, lowleveltypes::num_motors>>();
+                obj.torque_estimate = t[3].cast<std::array<float, lowleveltypes::num_motors>>();
+                return obj;
+            }
+        ))
+        .def("__copy__", [](const lowleveltypes::MotorState& obj) {
+            return lowleveltypes::MotorState(obj);
+        })
+        .def("__deepcopy__", [](const lowleveltypes::MotorState& obj, py::dict) {
+            return lowleveltypes::MotorState(obj);
+        }, "memo"_a);
 
     py::class_<lowleveltypes::MotorCommand>(m, "MotorCommand")
         .def(py::init<>())
